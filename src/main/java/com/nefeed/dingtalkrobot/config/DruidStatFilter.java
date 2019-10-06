871c7b63770d9f44a9e33d40e044bcaba31b6584
NeFeed <nefeed@163.com>
2019-10-06 10:40:13 +0800

    解决mybatis无法将mysql标准下划线命名字段映射到标准Java对象驼峰命名字段； 增加druid后台支持

---
+++ src/main/java/com/nefeed/dingtalkrobot/config/DruidStatFilter.java
@@ -0,0 +1,23 @@
+package com.nefeed.dingtalkrobot.config;
+
+import com.alibaba.druid.support.http.WebStatFilter;
+
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+
+/**
+ * @author 章华隽
+ * @mail nefeed@163.com
+ * @time 2019-10-06 10:33
+ */
+@WebFilter(filterName = "druidWebStatFilter", urlPatterns = "/*",
+
+        initParams = {
+
+                @WebInitParam(name = "exclusions", value = "*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*")
+
+        }
+)
+
+public class DruidStatFilter extends WebStatFilter {
+}
