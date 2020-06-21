7bee5863dcb523e93cf6344c8887607cf73663c8
NeFeed <nefeed@163.com>
2020-06-21 23:04:41 +0800

    添加新的获取节假日方法

--- src/main/java/com/nefeed/dingtalkrobot/utils/HolidayUtil.java
+++ src/main/java/com/nefeed/dingtalkrobot/utils/HolidayUtil.java
@@ -36,6 +36,7 @@ public class HolidayUtil {
 
     /**
      * 获取指定日期的类型
+     *
      * @param timestamp 时间戳(unixTimestamp秒数)
      * @return 指定日期的类型
      */
@@ -45,6 +46,7 @@ public class HolidayUtil {
 
     /**
      * 获取指定日期的类型
+     *
      * @param date 指定日期
      * @return 指定日期的类型
      */
@@ -53,24 +55,76 @@ public class HolidayUtil {
         if (holidayMap.containsKey(ymd)) {
             return holidayMap.get(ymd);
         }
-        HolidayEnum holidayEnum;
+        HolidayEnum holidayEnum = null;
         try {
-            holidayEnum = requestTimorHolidayEnum(ymd);
+            holidayEnum = requestKancloudHolidayEnum(ymd);
         } catch (Exception e) {
-            // 通过GoSeekCN进行补偿
-            holidayEnum = requestGoSeekCNHolidayEnum(ymd);
+            log.error("第一阶段获取节假日失败: {}", ymd);
+        }
+        if (null == holidayEnum) {
+            try {
+                holidayEnum = requestTimorHolidayEnum(ymd);
+            } catch (Exception e) {
+                // 通过GoSeekCN进行补偿
+                log.error("第二阶段获取节假日失败: {}", ymd);
+                holidayEnum = requestGoSeekCNHolidayEnum(ymd);
+            }
+            if (holidayEnum == null) {
+                log.error("第三阶段获取节假日失败: {}", ymd);
+            }
         }
         return holidayEnum;
     }
 
     /**
+     * 请求kancloud公开接口获取节假日类型(response 0工作日 1 假日 2节日)
+     *
+     * @param ymd 指定年月日
+     * @return 节假日类型
+     */
+    private static HolidayEnum requestKancloudHolidayEnum(String ymd) throws Exception {
+        String httpUrl = "http://tool.bitefu.net/jiari/?d=" + ymd;
+        String httpResult;
+        try {
+            httpResult = HttpUtil.get(httpUrl, null);
+        } catch (IOException e) {
+            e.printStackTrace();
+            log.error("请求Kancloud节假日信息异常!", e);
+            throw e;
+        }
+
+
+        HolidayEnum holidayEnum = null;
+        if ("0".equals(httpResult)) {
+            holidayEnum = HolidayEnum.NORMAL_WORKDAY;
+        } else if ("1".equals(httpResult)) {
+            holidayEnum = HolidayEnum.NORMAL_HOLIDAY;
+        } else if ("2".equals(httpResult)) {
+            holidayEnum = HolidayEnum.LEGAL_HOLIDAY;
+        }
+
+        if (holidayEnum == null) {
+            String errMsg = String.format("Kancloud节假日内部处理异常, 连接:[%s], 返回:[%s]",
+                    httpUrl, httpResult);
+            log.error(errMsg);
+            throw new Exception(errMsg);
+        }
+
+        holidayMap.put(ymd, holidayEnum);
+        log.info("获取Kancloud节假日类型成功，Map新增内容: ({}, {})", ymd, holidayEnum);
+        log.info("Kancloud节假日记录Map完整内容: [{}]", JSON.toJSONString(holidayMap));
+        return holidayEnum;
+    }
+
+    /**
      * 请求Timor公开接口获取节假日类型
+     *
      * @param date 指定年月日
      * @return 节假日类型
      */
     private static HolidayEnum requestTimorHolidayEnum(String date) throws Exception {
         StringBuilder sb = new StringBuilder();
-        sb.append(date, 0, 4).append("-").append(date, 4, 6).append( "-").append(date, 6, 8);
+        sb.append(date, 0, 4).append("-").append(date, 4, 6).append("-").append(date, 6, 8);
         String httpUrl = "http://timor.tech/api/holiday/info/" + sb.toString();
         String httpResult;
         try {
@@ -82,7 +136,7 @@ public class HolidayUtil {
         }
         TimorHolidayResponse timorHolidayResponse = JSON.parseObject(httpResult, TimorHolidayResponse.class);
         if (timorHolidayResponse == null || 0 != timorHolidayResponse.getCode()
-        || timorHolidayResponse.getType() == null) {
+                || timorHolidayResponse.getType() == null) {
             String errMsg = String.format("Timor节假日内部处理异常, 连接:[%s], 返回:[%s]",
                     httpUrl, httpResult);
             log.error(errMsg);
@@ -110,6 +164,7 @@ public class HolidayUtil {
 
     /**
      * 请求GoSeekCN公开接口获取节假日类型
+     *
      * @param date 指定年月日
      * @return 节假日类型
      */
@@ -143,6 +198,7 @@ public class HolidayUtil {
 
     /**
      * 是否是工作日
+     *
      * @param date 日期
      * @return boolean
      */
@@ -153,6 +209,7 @@ public class HolidayUtil {
 
     /**
      * 是否是工作日
+     *
      * @param holiday 节假日枚举
      * @return boolean
      */
@@ -162,6 +219,7 @@ public class HolidayUtil {
 
     /**
      * 是否是节假日
+     *
      * @param date 日期
      * @return boolean
      */
@@ -172,6 +230,7 @@ public class HolidayUtil {
 
     /**
      * 是否是节假日
+     *
      * @param holiday 节假日枚举
      * @return boolean
      */
@@ -181,6 +240,7 @@ public class HolidayUtil {
 
     /**
      * 今天和昨天是工作日,明天是节假日,定义为每周最后一个工作日
+     *
      * @param timestamp 指定日期
      * @return 是否为最后一个工作日
      * @throws IOException io异常
@@ -191,6 +251,7 @@ public class HolidayUtil {
 
     /**
      * 今天和昨天是工作日,明天是节假日,定义为每周最后一个工作日
+     *
      * @param date 指定日期
      * @return 是否为最后一个工作日
      * @throws IOException io异常
@@ -217,6 +278,7 @@ public class HolidayUtil {
 
     /**
      * 是否是周末
+     *
      * @param date 日期
      * @return boolean
      */
@@ -224,6 +286,6 @@ public class HolidayUtil {
         Calendar cal = Calendar.getInstance();
         cal.setTime(date);
         int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
-       return dayOfWeek == Calendar.SATURDAY || dayOfWeek == Calendar.SUNDAY;
+        return dayOfWeek == Calendar.SATURDAY || dayOfWeek == Calendar.SUNDAY;
     }
 }
