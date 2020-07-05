b72ae69851e4635d0ab909984f8180b371cc05b9
NeFeed <nefeed@163.com>
2020-07-05 23:41:40 +0800

    升级依赖第三方jar版本

--- src/main/java/com/nefeed/dingtalkrobot/DingtalkRobotApplication.java
+++ src/main/java/com/nefeed/dingtalkrobot/DingtalkRobotApplication.java
@@ -1,13 +1,19 @@
 package com.nefeed.dingtalkrobot;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * @author nefeed@163.com
+ */
 @SpringBootApplication
 @MapperScan("com.nefeed.dingtalkrobot.dao")
 @ComponentScan(basePackages = {"com.nefeed.dingtalkrobot.*"})
+@EnableAutoConfiguration(exclude = {DruidDataSourceAutoConfigure.class})
 public class DingtalkRobotApplication {
 
     public static void main(String[] args) {
